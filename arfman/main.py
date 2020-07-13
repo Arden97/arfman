1a2025b1ec6f92b533e61e9377f360c667e4723a
Artem Denisov <Artemden97@mail.ru>
2020-07-13 14:06:20 +0200

    update main

--- arfman/main.py
+++ arfman/main.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python
 from .utils import *
 
-#TODO autofill ?
-#TODO sort files?
-#TODO show/hide dotfiles
-#TODO replace
-#TODO copy directory
-
 def main():
     stdscr = init_screen()
     init_colors()
